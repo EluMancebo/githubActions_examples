510e26037d7f568ff27422dc3393e36faa7f2ab8
EluM <emango0298@gmail.com>
2024-11-30 20:30:02 +0100

    actualizar ficheros y añadir suma

---
+++ index.js
@@ -0,0 +1,13 @@
+//Importa Express
+const express = require("express");
+const app = express();
+//Define un puerto
+const PORT = 3037;
+//Ruta básica
+app.get("/", (req, res) => {
+  res.send("¡Hola, Express!");
+});
+//Inicia el servidor
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en http://localhost${PORT}");
+});
